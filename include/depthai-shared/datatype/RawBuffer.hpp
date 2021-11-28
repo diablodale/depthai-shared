2d396d134219ad11c8270a2da2c25b05f01b5606
Dale Phurrough <dale@hidale.com>
2021-11-28 05:56:30 +0100

    add align to rawbuffer vector

--- include/depthai-shared/datatype/RawBuffer.hpp
+++ include/depthai-shared/datatype/RawBuffer.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <boost/align/aligned_allocator.hpp>
 #include <cstdint>
 #include <nlohmann/json.hpp>
 #include <vector>
@@ -7,10 +8,14 @@
 
 namespace dai {
 
+// to match CV_MALLOC_ALIGN
+template <typename T, int ALIGN_BYTES = 64>
+using aligned_vector = std::vector<T, boost::alignment::aligned_allocator<T, ALIGN_BYTES>>;
+
 /// RawBuffer structure
 struct RawBuffer {
     virtual ~RawBuffer() = default;
-    std::vector<std::uint8_t> data;
+    dai::aligned_vector<std::uint8_t> data;
 
     virtual void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const {
         (void)metadata;
